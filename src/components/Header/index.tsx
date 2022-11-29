b13b11d335d9fad11be50eb93abf20ca5b1f81df
wesley_wisch <wesley_wisch@hotmail.com>
2022-11-29 15:05:12 -0300

    feat: Already persisting user data and recovering them if he leaves the page, and if the user is admin he can add a new item.

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,6 @@
 import { Link } from 'react-router-dom';
 import { motion } from 'framer-motion';
-import { MdShoppingBasket } from 'react-icons/md';
+import { MdShoppingBasket, MdAdd, MdLogout } from 'react-icons/md';
 
 import Logo from '../../assets/logo.png';
 import Avatar from '../../assets/avatar.png';
@@ -8,7 +8,7 @@ import Avatar from '../../assets/avatar.png';
 import { useUser } from '../../hooks/useUser';
 
 export function Header() {
-  const { setUser } = useUser();
+  const { setUser, user } = useUser();
 
   const menuItems = [
     { name: 'Home' },
@@ -18,16 +18,19 @@ export function Header() {
   ];
 
   function login() {
-    const userFakeTeste = {
-      displayName: 'Wesley Wisch',
-      email: 'wesley_wisch@hotmail.com',
-      photoURL: 'https://github.com/wesleywisch.png',
-      providerId: 'Google.com',
-      uid: '7427483269bfjsdjlbfkjb238y482',
-    }
+    if (!user?.displayName) {
+      const userFakeTeste = {
+        displayName: 'Wesley Wisch',
+        email: 'wesley_wisch@hotmail.com',
+        photoURL: 'https://github.com/wesleywisch.png',
+        providerId: 'Google.com',
+        uid: '7427483269bfjsdjlbfkjb238y482',
+        admin: true,
+      }
 
-    setUser(userFakeTeste);
-    console.log('Fazer essa funcionalidade')
+      setUser(userFakeTeste);
+      console.log('Fazer essa funcionalidade')
+    }
   }
 
   return (
@@ -64,11 +67,27 @@ export function Header() {
           <div className="relative">
             <motion.img
               whileTap={{ scale: 0.6 }}
-              className="w-10 min-w-[40px] h-10 min-h-[40px] drop-shadow-xl cursor-pointer"
-              src={Avatar}
-              alt="Imagem do usuário"
+              className="w-10 min-w-[40px] h-10 min-h-[40px] drop-shadow-xl cursor-pointer rounded-full"
+              src={user?.photoURL ? user.photoURL : Avatar}
+              alt={user?.displayName ? user.displayName : 'Avatar'}
               onClick={login}
             />
+
+            <div className="w-40 bg-gray-50 shadow-xl rounded-lg flex flex-col absolute top-12 right-0">
+              {user && user.admin === true && (
+                <Link
+                  to='/createItem'
+                  className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base"
+                >
+                  New Item <MdAdd />
+                </Link>
+              )}
+              <p
+                className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base"
+              >
+                Logout <MdLogout />
+              </p>
+            </div>
           </div>
         </div>
       </div>
--- src/contexts/UserContext.tsx
+++ src/contexts/UserContext.tsx
@@ -1,14 +1,52 @@
-import { createContext, Dispatch, SetStateAction, useState } from "react";
+import { createContext, Dispatch, SetStateAction, useEffect, useState } from "react";
+
+interface User {
+  displayName: string;
+  email: string;
+  photoURL: string;
+  providerId: string;
+  uid: string;
+  admin?: boolean;
+}
 
 export interface UserContextDataProps {
-  user: {};
-  setUser: Dispatch<SetStateAction<{}>>;
+  user: User | null;
+  setUser: Dispatch<SetStateAction<User>>;
 }
 
 export const UserContext = createContext({} as UserContextDataProps);
 
 export function UserContextProvider({ children }: { children: React.ReactNode }) {
-  const [user, setUser] = useState({});
+  function setDataInLocalStorage(name: string, data: any) {
+    localStorage.setItem(name, JSON.stringify(data));
+  }
+
+  function getDataInLocalStorage(name: string) {
+    const storage = localStorage.getItem(name);
+
+    if (storage) {
+      return storage;
+    }
+
+    return null
+  }
+
+  const [user, setUser] = useState<User>(() => {
+    const userInStorage = getDataInLocalStorage('user');
+
+    if (userInStorage) {
+      return JSON.parse(userInStorage);
+    }
+
+    return {} as User;
+  });
+
+
+  useEffect(() => {
+    if (user.displayName) {
+      setDataInLocalStorage('user', user);
+    }
+  }, [user])
 
   return (
     <UserContext.Provider value={{
